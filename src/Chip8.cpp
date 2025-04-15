3f85612e4e58bbb67778035a4d8d82586bcc7e4a
Eliott <yot360@users.noreply.github.com>
2025-04-15 02:26:14 +0000

    parsing instructions from memory as 16-bit

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -1,7 +1,7 @@
 #include "Chip8.h"
 
 Chip8::Chip8(std::string rom_path) :
-	m_memory{}, m_pixels{}
+	m_memory{}, m_pixels{}, m_pc(512)
 {
 	// Copy font to memory
 	std::copy(FONT.begin(), FONT.end(), m_memory.begin() + 80);
@@ -24,6 +24,12 @@ void Chip8::update()
 	if (m_sound_timer > 0) {
 		m_sound_timer--;
 	}
+
+	// Fetch instructions that PC is currently pointing at from memory.
+	uint16_t instruction = (m_memory[m_pc] << 8) | m_memory[m_pc + 1]; // Example : m_memory[m_pc] = 0xAB = 10101011    we move this 8 bits to the left so we have 10101011 00000000    then we can put the m_memory[m_pc+1] to the right to have 10101011 11001101
+	if (instruction != 0)
+		std::cout << std::hex << instruction << std::endl;
+	m_pc += 2;
 }
 
 Chip8::~Chip8()
